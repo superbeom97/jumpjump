1f12442f282d73c0d0350bf642fef9f7540ca968
USER9-PC <you@example.com>
2017-11-24 15:23:14 +0900

    작업1

---
+++ Chap02/02_.py
@@ -0,0 +1,20 @@
+# 02-2 문자열 자료형
+
+# 문자열 더해서 연결하기(Concatenation)
+head="Python"
+tail=" is fun!"
+head+tail
+print(head+tail)
+'Python is fun!'
+
+# 슬라이싱으로 문자열 나누기
+a="20171124Cloudy"
+year=a[:4]
+day=a[4:8]
+weather=a[8:]
+print(year)
+2017
+print(day)
+1124
+print(weather)
+'Cloudy'
