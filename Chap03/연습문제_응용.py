55470e4c695bb3ab94040a591f5a8d634862ba45
USER9-PC <you@example.com>
2017-11-28 16:33:35 +0900

    수업_17.11.28

--- Chap03/연습문제_응용.py
+++ Chap03/연습문제_응용.py
@@ -6,13 +6,17 @@
 #                 ***
 #                  *
 
-i=0
 while True:
-    number = int(input("홀수를 입력하세요(프로그램 종료:0)"))
-    i+=1
-    if i>=1:
-        print("*"*i)
-    elif i / 2 ==0: continue
+    number = int(input("홀수를 입력하세요(프로그램 종료:0):"))
+    if not number==0:
+        if number>=1 and number%2!=0:
+            print("%s %s %s" %(''*(number-2),'*',''*(number-2)))
+            print("%s" %'*'*number)
+        else: continue
     else:
-        print("마름모 연습프로그램을 이용해 주셔서 감사합니다.")
-        break
+        if number==0:
+            print("마름모 연습프로그램을 이용해 주셔서 감사합니다.")
+            break
+
+
+# i, i
