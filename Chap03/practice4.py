5b754b6bbfb690fd0b9a1fde4a977fcc605749a9
USER9-PC <you@example.com>
2017-11-28 15:12:13 +0900

    수업_17.11.28

--- Chap03/practice4.py
+++ Chap03/practice4.py
@@ -8,7 +8,7 @@ while True:
         select = int(input("1: 현금, 2: 공원전용 신용카드:"))
         if select==2:
             print("%s원 결제되었습니다. 티켓을 발행합니다." %(2000*0.9))
-        else:
+        elif select==1:
          charge = int(input("요금을 입력하세요:"))
          if charge <=1999:
             print("%s원이 모자랍니다. 입력하신 %s원을 반환합니다." %(2000-charge,charge))
@@ -16,12 +16,13 @@ while True:
             print("감사합니다. 티켓을 발행합니다.")
          else:
             print("감사합니다. 티켓을 발행하고 거스름돈 %s원을 반환합니다." %(charge-2000))
+        else: continue
     elif age >=14 and age <=18:
         print("귀하는 %s등급이며, 요금은 %s입니다." %('청소년','3000원'))
         select = int(input("1: 현금, 2: 공원전용 신용카드:"))
         if select==2:
             print("%s원 결제되었습니다. 티켓을 발행합니다." %(3000*0.9))
-        else:
+        elif select==1:
          charge = int(input("요금을 입력하세요:"))
          if charge <=2999:
             print("%s원이 모자랍니다. 입력하신 %s원을 반환합니다." %(3000-charge,charge))
@@ -29,12 +30,13 @@ while True:
             print("감사합니다. 티켓을 발행합니다.")
          else:
             print("감사합니다. 티켓을 발행하고 거스름돈 %s원을 반환합니다." %(charge-3000))
+        else: continue
     elif age >=19 and age <=59:
         print("귀하는 %s등급이며, 요금은 %s입니다." %('성인','5000원'))
         select = int(input("1: 현금, 2: 공원전용 신용카드:"))
         if select==2:
             print("%s원 결제되었습니다. 티켓을 발행합니다." %(5000*0.9))
-        else:
+        elif select==1:
          charge = int(input("요금을 입력하세요:"))
          if charge <4999:
             print("%s원이 모자랍니다. 입력하신 %s원을 반환합니다." %(5000-charge,charge))
@@ -42,12 +44,13 @@ while True:
             print("감사합니다. 티켓을 발행합니다.")
          else:
             print("감사합니다. 티켓을 발행하고 거스름돈 %s원을 반환합니다." %(charge-5000))
+        else: continue
     elif age >=60 and age <=65:
         print("귀하는 %s등급이며, 요금은 %s입니다." %('성인','5000원'))
         select = int(input("1. 현금, 2: 공원전용 신용카드:"))
         if select==2:
-            print("%s원 결제되었습니다. 티켓을 발행합니다." %(5000*0.85))
-        else:
+            print("%s원 결제되었습니다. 티켓을 발행합니다." %(5000*0.9*0.95))
+        elif select==1:
             charge = int(input("요금을 입력하세요:"))
             if charge < 4999:
                 print("%s원이 모자랍니다. 입력하신 %s원을 반환합니다." % (5000 - charge, charge))
@@ -55,6 +58,7 @@ while True:
                 print("감사합니다. 티켓을 발행합니다.")
             else:
                 print("감사합니다. 티켓을 발행하고 거스름돈 %s원을 반환합니다." % (charge - 5000))
+        else: continue
     else:
         print("귀하는 %s등급이며, 요금은 %s입니다." %('노인','무료'))
         print("감사합니다. 티켓을 발행합니다.")
