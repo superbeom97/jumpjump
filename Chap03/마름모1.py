3f7b80830b7e81e1e522f664c65c2285b3c8db11
USER9-PC <you@example.com>
2017-11-29 16:21:10 +0900

    수업_17.11.29

--- Chap03/마름모1.py
+++ Chap03/마름모1.py
@@ -1,16 +1,16 @@
 star='*'
-blank=" "
-#line=
+blank= ""
 while True:
-    customer = int(input("홀수를 입력하세요(프로그램 종료=0):"))
-    if customer >= 1 and customer % 2 != 0:
-        for i in range(10):
-            print(i*star, end=" ")
-        print('')
-    elif customer % 2 ==0 and customer != 0:
+    print("<<마름모 프로그램 연습 ver1.0>>")
+    number = int(input("홀수를 입력하세요(프로그램 종료=0):"))
+    if number >= 1 and number % 2 != 0:
+        for i in range(number+1):
+            if i % 2 != 0:
+                print(i*star)
+    elif number % 2 ==0 and number != 0:
         print("짝수를 입력하셨습니다. 다시 입력하세요")
         continue
     else:
-        if customer == 0:
+        if number == 0:
             print("마름모 연습프로그램을 이용해 주셔서 감사합니다.")
             break
---
+++ Chap03/마름모_정답.py
@@ -0,0 +1,21 @@
+star = '*'
+blank = " "
+while True:
+    print("<<마름모 프로그램 연습 ver1.0>>")
+    number = int(input("홀수를 입력하세요(프로그램 종료=0):"))
+    if number == 0:
+        print("마름모 연습프로그램을 이용해 주셔서 감사합니다.")
+        break
+    elif number % 2 == 0:
+        print("짝수를 입력하셨습니다. 다시 입력하세요")
+        continue
+    else:
+        star_count = 1
+        blank_count = int((number - star_count)/2)
+        while True:
+            print(blank*blank_count, end="")
+            print(star*star_count)
+            if number == star_count:
+                break
+            star_count += 2
+            blank_count -= 1
