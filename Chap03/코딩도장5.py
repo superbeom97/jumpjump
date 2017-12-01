add5468e3f8314d4a196f4f31193adafcf9c254b
USER9-PC <you@example.com>
2017-12-01 17:25:45 +0900

    수업_17.11.30

---
+++ Chap03/코딩도장5.py
@@ -0,0 +1,6 @@
+special = [-1, 1, 3, -2, 2]
+
+# special의 값 > 0 : 왼쪽 정렬 // sepcial의 값 > 0 : 오른쪽 정렬
+
+#for a in special:
+   # if a < 0:
---
+++ Chap03/코딩도장6.py
@@ -0,0 +1,7 @@
+c = ['1238888188008']
+c = str(c)
+print(c.count('1'))
+
+number = range(1, 10001)
+list = str(number)
+print(list.count('8'))
