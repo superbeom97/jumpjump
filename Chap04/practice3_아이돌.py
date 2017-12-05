cebd36db915eb4ea632b900cd40e1af35842798c
IoT_class_beom <beom9790@naver.com>
2017-12-05 17:25:30 +0900

    수업_17.12.04

--- Chap04/practice3_아이돌.py
+++ Chap04/practice3_아이돌.py
@@ -3,8 +3,8 @@ show_list = f.read()
 print(show_list, "\n")
 f.close()
 
-make_idol = show_list.split()
-for i in make_idol:
+make_idol = show_list.split() # split 개념을 생각하지 못했었다. f.read()로 불렀으니 하나의 문자열이야
+for i in make_idol:          # 그걸 하나하나 나눠 주기 위해 split 사용!!
     print("신예 아이돌 " + i + " 인기 급상승")
 print("\n", end="")
 
