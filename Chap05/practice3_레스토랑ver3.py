a00d45e3dc404bbf1af1f7106b0ab0b0615a92b0
IoT_class_beom <beom9790@naver.com>
2017-12-06 15:42:14 +0900

    수업_17.12.04

--- Chap05/practice3_레스토랑ver3.py
+++ Chap05/practice3_레스토랑ver3.py
@@ -1,7 +1,7 @@
 class Restaurant:
     cuisine_type = "대구 전통의 맛"
     restaurant_name = "복복복을 먹자"
-    number_served = 0
+    number_served = 0 # 이 변수, 어렵다!
 
     def __init__(self, name, type):
         self.restaurant_name = name
@@ -14,13 +14,18 @@ class Restaurant:
         print("저희 %s 레스토랑 오픈했습니다. 어서오세요" % self.restaurant_name)
 
     def set_number_serced(self, number):
-        print("새로운 손님 %s명 들어오십니다." % number)
+        self.number_served = number # 이 변수를 몰라서 못 넣었다! 추가해주는 거 잊지 말고~
+        print("방문한 손님을 %s명으로 수정하였습니다." % self.number_served)
 
     def increment_number_served(self, number):
-        print("오늘의 손님 %s명 째입니다." % number)
+        self.number_served += number # 이 변수를 몰라서 못 넣었다! 추가해주는 거 잊지 말고~
+        print("오늘의 총 손님 수는 %s명입니다." % self.number_served)
 
 A_restaurant = Restaurant("'복복복을 먹자'", "'대구 전통의 맛'")
 A_restaurant.describe_restaurant()
 A_restaurant.open_restaurant()
-A_restaurant.set_number_serced(7)
-A_restaurant.increment_number_served(10)
+
+A_restaurant.set_number_serced(5)  # 손님 수를 임의로 설정해서 넣어 준 뒤
+print(A_restaurant.number_served) # 확인하고 싶으면 이렇게 print 함수를 사용해서 확인!!
+A_restaurant.increment_number_served(5)
+print(A_restaurant.number_served) # 확인하고 싶으면 이렇게 print 함수를 사용해서 확인!!
