5d6340ffa6fd1fa2b6ea00eff1a86e9a42ae2fab
IoT_class_beom <beom9790@naver.com>
2017-12-08 11:50:00 +0900

    수업_17.12.04

--- Chap05/practice1_극장.py
+++ Chap05/practice1_극장.py
@@ -1,11 +1,14 @@
 while True:
     age = int(input("나이를 입력해 주세요:)\n종료를 원하시면 0을 입력해 주세요 :"))
+
     if age < 0:
         print("나이를 올바르게 입력해 주세요")
         continue
+
     elif age == 0:
         print("이용해 주셔서 감사합니다.")
         break
+
     else:
         if 1 <= age <= 2:
             print("반갑습니다. 무료로 입장하시면 됩니다!")
