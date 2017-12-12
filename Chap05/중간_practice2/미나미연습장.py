750f6f423b4d14e96b11068ad8f417a71dc93bbc
IoT_class_beom <beom9790@naver.com>
2017-12-12 17:04:04 +0900

    수업_17.12.11

--- Chap05/중간_practice2/미나미연습장.py
+++ Chap05/중간_practice2/미나미연습장.py
@@ -2,29 +2,30 @@ def sum(a, b):
     return a + b
 
 while True:
-    try:
         number = input("어서옵쇼. 두 수를 입력하세요 (종료를 원하시면 1을 입력해 주세요): ")
-        berry = number.split()
-        # one = int(berry[0])
-        # two = int(berry[1])
 
-        if berry[0] and berry[1]:
-            print(sum(int(berry[0]), int(berry[1])))
-            continue
-
-        elif int(berry[0]) == 1 and berry[1] == " ":
+        if number == "종료":
             print("이용해 주셔서 감사합니다.")
             break
 
-    except:
-        if int(berry[0]) and berry[1]:
-            print("죄송합니다. 두 번째 입력이 %s입니다. 숫자를 입력하세요." % berry[1])
-            continue
-    #
-    #     elif one_str and two:
-    #         print("죄송합니다. 첫 번째 입력이 %s입니다. 숫자를 입력하세요." % one_str)
-    #         continue
-    #
-    #     elif one_str and two_str:
-    #         print("죄송합니다. 첫 번째 입력이 %s이고 두 번째 입력이 %s입니다. 숫자를 입력하세요." % (one_str, two_str))
-    #         continue
+        else:
+            berry = number.split()
+            one = berry[0]
+            two = berry[1]
+
+            try:
+                one and two
+                print(sum(int(one), int(two)))
+                # continue # try 자체가 독립적이니 continue 할 필요 없는
+            except:
+                pass
+
+            try:
+                int(one) and two
+            except:
+                print("죄송합니다. 첫 번째 입력이 %s입니다. 숫자를 입력하세요" % one)
+
+            try:
+                one and int(two)
+            except:
+                print("죄송합니다. 두 번째 입력이 %s입니다. 숫자를 입력하세요" % two)
--- Chap05/중간_practice2/연습문제4.py
+++ Chap05/중간_practice2/연습문제4.py
@@ -1,28 +1,32 @@
-# 아직 미완성!!!!!!!!
 def sum(a, b):
     return a + b
 
+
 while True:
-    try:
-        number = input("어서옵쇼. 두 수를 입력하세요 (종료를 원하시면 1을 입력해 주세요): ")
+    number = input("어서옵쇼. 두 수를 입력하세요 (종료를 원하시면 1을 입력해 주세요): ")
+
+    if number == "종료":
+        print("이용해 주셔서 감사합니다.")
+        break
+
+    else:
         berry = number.split()
-        one = int(berry[0])
-        two = int(berry[1])
+        one = berry[0]
+        two = berry[1]
 
-        if one and two:
-            print(sum(one, two))
-            continue
+        try:
+            one and two
+            print(sum(int(one), int(two)))
+            # continue # try 자체가 독립적이니 continue 할 필요 없는
+        except:
+            pass
 
-        elif one == 1:
-            print("이용해 주셔서 감사합니다.")
-            break
+        try:
+            int(one) and two
+        except:
+            print("죄송합니다. 첫 번째 입력이 %s입니다. 숫자를 입력하세요" % one)
 
-    except:
-        if one and two:
-            print("죄송합니다. 두 번째 입력이 %s입니다. 숫자를 입력하세요." % two)
-
-        elif str(one) and two:
-            print("죄송합니다. 첫 번째 입력이 %s입니다. 숫자를 입력하세요." % str(one))
-
-        elif str(one) and str(two):
-            print("죄송합니다. 첫 번째 입력이 %s이고 두 번째 입력이 %s입니다. 숫자를 입력하세요." % (str(one), str(two)))
+        try:
+            one and int(two)
+        except:
+            print("죄송합니다. 두 번째 입력이 %s입니다. 숫자를 입력하세요" % two)
