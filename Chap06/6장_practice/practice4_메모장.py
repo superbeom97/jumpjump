621dc3dbda5764763112fd8baf90504ef30c1580
IoT_class_beom <beom9790@naver.com>
2017-12-18 12:03:36 +0900

    수업_17.12.15

--- Chap06/6장_practice/practice4_메모장.py
+++ Chap06/6장_practice/practice4_메모장.py
@@ -6,12 +6,13 @@ args_two = args[2:]
 
 if args_one == "-a":
     try:
+        f = open("memo.txt", 'r')
         f = open("memo.txt", 'a')
         f.write(" ".join(args_two))
         f.write("\n")
         f.close()
 
-    except FileNotFoundError:
+    except:
         number = int(input("memo.txt 파일이 없습니다. 아래 중 선택하세요\n1. memo.txt 파일을 새로 생성하시겠습니까?\n2. 파일 경로를 입력하시겠습니까?\n: "))
 
         if number == 1:
@@ -36,17 +37,18 @@ elif args_one == "-au":
     for i in args_two:
         f = open("memo.txt", 'a')
         b = "".join(i)
-        # f.write("\n")
-        # print(i.upper(), end=" ")
-        f.write(b.upper()+"\n")
+        f.write(b.upper())
+        f.write("\n")
         f.close
 
 elif args_one == "-v":
     try:
         f = open("memo.txt", 'r')
+        data = f.read()
+        print(data)
 
     except FileNotFoundError:
-        number = int(input("memo.txt 파일이 없습니다. bb아래 중 선택하세요\n1. 종료하시겠습니까?\n2. 파일 경로를 입력하시겠습니까?\n: "))
+        number = int(input("memo.txt 파일이 없습니다. 아래 중 선택하세요\n1. 종료하시겠습니까?\n2. 파일 경로를 입력하시겠습니까?\n: "))
 
         if number == 1:
             print("이용해 주셔서 감사합니다.")
@@ -54,7 +56,9 @@ elif args_one == "-v":
 
         elif number == 2:
             address = str(input("파일 경로를 입력하세요: "))
-            print("정상 처리되었습니다. 감사합니다.")
+            f = open(address, 'r')
+            data = f.read()
+            print(data)
 
         else:
             print("1번과 2번 중에 선택해라잉 확 마")
