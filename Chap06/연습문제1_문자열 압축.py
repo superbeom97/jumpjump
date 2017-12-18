b9dec3bb90897ecca7c8962958385f7b5da03c11
IoT_class_beom <beom9790@naver.com>
2017-12-18 16:22:50 +0900

    수업_17.12.18

--- Chap06/연습문제1_문자열 압축.py
+++ Chap06/연습문제1_문자열 압축.py
@@ -1,12 +1,20 @@
-# 아직 미완성!!!!!!
-while True:
-    let = input("문자를 입력하세요: ") # "aaabbcccccca"
-    let_list = list(let) # ['a', 'a', 'a', 'b', 'b', 'c', 'c', 'c', 'c', 'c', 'c', 'a']
+def letter():
+    let = input("문자를 입력하세요: ")
+    blank_let = ""
+    result = ""
+    count = 0
 
-    i = 0
-    for j in let_list:
-        if let_list[j] == let_list[j+1]:
-            b = let_list[j] + let_list[j+1]
-            print(b)
+    for i in let:
+        if i != blank_let:
+            blank_let = i
+            count = 1
+            result = result + blank_let + str(count)
+
         else:
-            continue
+            count += 1
+            result = result + "\b" + str(count)
+
+    return  result
+
+while True:
+    print(letter())
