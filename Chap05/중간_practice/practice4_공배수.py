f31708ffe0c9341db71ac322cb21a27ab3a0e679
IoT_class_beom <beom9790@naver.com>
2017-12-18 16:59:18 +0900

    수업_17.12.18

--- Chap05/중간_practice/practice4_공배수.py
+++ Chap05/중간_practice/practice4_공배수.py
@@ -1,4 +1,4 @@
-## 공배수 개념 제대로 익히고 다시 하기!!
+## 공배수 개념 제대로 익히기!!
 while True:
     number = str(input("세 개의 양수를 입력하세요\n종료를 원하시면 -1을 입력하세요: "))
     john = "".join(number)
@@ -18,10 +18,20 @@ while True:
         two = int(check[1])
         three = int(check[2])
 
-        if (one * two) % three == 0:
-            print("%s는 %s와 %s의 공배수입니다." % (three, one, two))
-            continue
+        if one > two:
+            if three % ((one * two) / two) == 0: # (작은 수 x 큰 수) / 작은 수 = 최소 공배수 인 듯!!!!
+                print("%s는 %s와 %s의 공배수입니다." % (three, one, two))
+                continue
 
-        elif (one * two) % three != 0:
-            print("%s는 %s와 %s의 공배수가 아닙니다." % (three, one, two))
-            continue
+            else:
+                print("%s는 %s와 %s의 공배수가 아닙니다." % (three, one, two))
+                continue
+
+        else:
+            if three % ((one * two) / one) == 0: # (작은 수 x 큰 수) / 작은 수 = 최소 공배수 인 듯!!!
+                print("%s는 %s와 %s의 공배수입니다." % (three, one, two))
+                continue
+
+            else:
+                print("%s는 %s와 %s의 공배수가 아닙니다." % (three, one, two))
+                continue
