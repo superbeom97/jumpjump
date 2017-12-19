11d3e9f61089dd37345bb7b6183fe76f925c7099
IoT_class_beom <beom9790@naver.com>
2017-12-19 14:18:46 +0900

    수업_17.12.19

---
+++ 코딩도장_/No6_숫자 개수 구하기.py
@@ -0,0 +1,22 @@
+# 1~1000에서 각 숫자의 개수 구하기
+#
+# 예로 10 ~ 15 까지의 각 숫자의 개수를 구해보자
+#
+# 10 = 1, 0
+# 11 = 1, 1
+# 12 = 1, 2
+# 13 = 1, 3
+# 14 = 1, 4
+# 15 = 1, 5
+#
+# 그러므로 이 경우의 답은 0:1개, 1:7개, 2:1개, 3:1개, 4:1개, 5:1개
+
+def Count_Number(number):
+    total_num = ""
+    for i in range(1, number+1):
+        total_num += str(i)
+
+    for j in range(10):
+        print(total_num.count(str(j)))
+
+Count_Number(1000)
---
+++ 코딩도장_/No7_숫자 분해, 곱, 합.py
@@ -0,0 +1,24 @@
+# 10~1000까지 각 숫자 분해하여 곱하기의 전체 합 구하기
+#
+# 예로, 10~15까지의 각 숫자 분해하여 곱하기의 전체 합은 다음과 같다.
+#
+# 10 = 1 * 0 = 0
+#  11 = 1 * 1 = 1
+#  12 = 1 * 2 = 2
+#  13 = 1 * 3 = 3
+#  14 = 1 * 4 = 4
+#  15 = 1 * 5 = 5
+#
+# 그러므로, 이 경우의 답은 0+1+2+3+4+5 = 15
+
+
+
+def Count_Number(number):
+    total_num = ""
+    for i in range(1, number+1):
+        total_num += str(i)
+
+    for j in range(10):
+        print(total_num.count(str(j)))
+
+Count_Number(1000)
