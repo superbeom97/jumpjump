aa7fd5f16a312205c5e2533cf0ef300fcfaff35d
IoT_class_beom <beom9790@naver.com>
2017-12-20 12:23:09 +0900

    수업_17.12.19

---
+++ 코딩도장_/오늘의연습장1.py
@@ -0,0 +1,4 @@
+Alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+print(type(Alphabet[0]))
