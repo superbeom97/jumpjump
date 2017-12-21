1957b0ab94762ffe9b51bc49425edc2117081162
IoT_class_beom <beom9790@naver.com>
2017-12-21 17:28:48 +0900

    수업_17.12.21

--- 코딩도장_/연습장2.py
+++
@@ -1,3 +0,0 @@
-initial_number = 0
-add_number = ['1','2']
-print(type(len(add_number)))
--- 코딩도장_/오늘의연습장1.py
+++ 코딩도장_/오늘의연습장1.py
@@ -1,15 +1,3 @@
-n = 22
-add_number = []
-add_number.append(n)
-while True:
-    if n != 1:
-        if n % 2 == 0:
-            n = n / 2
-            add_number.append(int(n))
-        else:
-            n = (n * 3) + 1
-            add_number.append(int(n))
-    else:
-        print(add_number)
-        print(len(add_number))
-        break
+a = [1,2,3]
+b = [3]
+print()
