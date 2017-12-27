8a3c6e15fbe6b5272d1883738d1da7eb5b231cc9
IoT_class_beom <beom9790@naver.com>
2017-12-27 11:08:15 +0900

    수업_17.12.27

--- Data_Science/BigData_Source1.py
+++ Data_Science/BigData_Source1.py
@@ -17,6 +17,8 @@ countParticipants = []
 for row in data[1:]:
     countParticipants.append(int(row[countParticipantsIndex]))
 
+print("The contents of 'COUNT PARTICIPANTS': %s" % countParticipants)
+
 
 countCitizenStatusTotalIndex = data[0].index("COUNT CITIZEN STATUS TOTAL")
 print("The index of 'COUNT CITIZEN STATUS TOTAL': %s" % countCitizenStatusTotalIndex)
@@ -27,3 +29,14 @@ for row in data[1:]:
     countCitizenStatusTotal.append(int(row[countCitizenStatusTotalIndex]))
 
 print("The contents of 'COUNT CITIZEN STATUS TOTAL': %s" % countCitizenStatusTotal)
+
+
+countFemaleIndex = data[0].index("COUNT FEMALE")
+print("The index of 'COUNT FEMALE': %s" % countFemaleIndex)
+
+countFemale = []
+
+for row in data[1:]:
+    countFemale.append(int(row[countFemaleIndex]))
+
+print("The contents of 'COUNT FEMALE': %s" % countFemale)
