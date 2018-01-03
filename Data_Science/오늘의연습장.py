6f48edf06fdce475f58fa1244b4aad2b3b48171b
IoT_class_beom <beom9790@naver.com>
2018-01-03 16:27:00 +0900

    수업_17.12.28

--- Data_Science/오늘의연습장.py
+++ Data_Science/오늘의연습장.py
@@ -1,5 +1,10 @@
-i = 0
-while True:
-    i += 1
-    if i > 5 : break
-    print(i*"*")
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def adder(self, num):
+        self.result += num
+        print(self.result)
+
+cal1 = Calculator()
+cal1.adder(5)
