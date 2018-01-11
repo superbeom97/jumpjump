efc88793d0243d86f2e4523679b0b78fb0418e6c
IoT_class_beom <beom9790@naver.com>
2018-01-11 17:17:30 +0900

    수업_18.01.11

--- XML_/Movie_Rank.py
+++ XML_/Movie_Rank.py
@@ -10,22 +10,21 @@ movie_changes = soup.find_all('td', attrs={'class':'range ac'})
 a_result = []
 for z in movie_changes:
     td_z = list(z.strings)
-    a_result.append(td_z)
+    a_result.append(td_z[0])
 # a_result = a_result[]
-print(a_result)
 
-# movie_name_result = []
-# rank = 1
-# while True:
-#     for movie_name in movie_names:
-#         td_movie_name = list(movie_name.strings)
-#         td_movie_name[0] = rank
-#         for movie_chg in a_result:
-#             td_movie_name[2] = movie_chg
-#             movie_name_result.append(td_movie_name)
-#             rank += 1
-#             break
-#     break
-#
-# movie_table = DataFrame(movie_name_result, columns=('순위','영화명','변동폭'))
-# movie_table.to_csv("movie_table.csv", encoding="cp949", mode='w', index=False)
+movie_name_result = []
+rank = 1
+while True:
+    for movie_name in movie_names:
+        td_movie_name = list(movie_name.strings)
+        td_movie_name[0] = rank
+        for movie_chg in a_result:
+            td_movie_name[2] = movie_chg
+            movie_name_result.append(td_movie_name)
+            rank += 1
+            break
+    break
+
+movie_table = DataFrame(movie_name_result, columns=('순위','영화명','변동폭'))
+movie_table.to_csv("movie_table.csv", encoding="cp949", mode='w', index=False)
