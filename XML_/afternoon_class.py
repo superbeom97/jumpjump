a0ed062d6e81abc1b261f9a9d55c13f8bf303a32
IoT_class_beom <beom9790@naver.com>
2018-01-11 17:13:33 +0900

    수업_18.01.11

---
+++ XML_/afternoon_class.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+
+html='<td class="title"><div class="tit3"><a href="/movie/bi/mi/basic.nhn?code=158191" title="1987">1987</a></div></td>'
+
+# <a title  마우스 타겟 시 설명 메세지 출력
+
+soup = BeautifulSoup(html,'html.parser')
+# print(soup)
+tag = soup.td
+# print(tag)
+tag = soup.div
+# print(tag)
+tag = soup.a
+print(tag)
+# print(tag.name)
+# print(tag.attrs)
+# print(tag.string)
+# print(tag.text)
