79cb6ce0c7e859dd58280571b0ae42f80834327a
subeom_home <seho1009@naver.com>
2018-01-11 20:27:46 +0900

    18.01.11_home

--- XML_/Movie_Rank_ver1.py
+++ XML_/Movie_Rank_ver1.py
@@ -7,23 +7,23 @@ soup = BeautifulSoup(html,'html.parser')        ## 모든 소스코드를 따오
 
 movie_names = soup.find_all('div', attrs={'class':'tit3'})
 movie_changes = soup.find_all('td', attrs={'class':'range ac'})
-a_result = []
-for z in movie_changes:
-    td_z = list(z.strings)
-    a_result.append(td_z[0])
-# a_result = a_result[]
+movie_changes_result = []
+for movie_change in movie_changes:
+    td_movie_change = list(movie_change.strings)
+    movie_changes_result.append(td_movie_change[0])
 
 movie_name_result = []
 rank = 1
+movie_changes_index = 0
 while True:
     for movie_name in movie_names:
         td_movie_name = list(movie_name.strings)
         td_movie_name[0] = rank
-        for movie_chg in a_result:
-            td_movie_name[2] = movie_chg
-            movie_name_result.append(td_movie_name)
-            rank += 1
-            break
+        movie_chg = movie_changes_result[int("%s" % movie_changes_index)]
+        td_movie_name[2] = movie_chg
+        movie_name_result.append(td_movie_name)
+        rank += 1
+        movie_changes_index += 1
     break
 
 movie_table = DataFrame(movie_name_result, columns=('순위','영화명','변동폭'))
