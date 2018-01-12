c3c7e0f6422e4105ded0a112f79018f187bf63db
IoT_class_beom <beom9790@naver.com>
2018-01-12 12:32:27 +0900

    수업_18.01.12

--- XML_/practice2_Movie_Rank_ver1.py
+++ XML_/practice2_Movie_Rank_ver1.py
@@ -5,8 +5,8 @@ from pandas import DataFrame                ## HTML로 바꾼 코드를 테이�
 html = urllib.request.urlopen('http://movie.naver.com/movie/sdb/rank/rmovie.nhn')
 soup = BeautifulSoup(html,'html.parser')        ## 모든 소스코드를 따오는
 
-movie_names = soup.find_all('div', attrs={'class':'tit3'})
-movie_changes = soup.find_all('td', attrs={'class':'range ac'})
+movie_names = soup.findAll('div', attrs={'class':'tit3'})
+movie_changes = soup.findAll('td', attrs={'class':'range ac'})
 movie_changes_result = []
 for movie_change in movie_changes:
     td_movie_change = list(movie_change.strings)
@@ -18,8 +18,7 @@ movie_changes_index = 0
 for movie_name in movie_names:
     td_movie_name = list(movie_name.strings)
     td_movie_name[0] = rank
-    movie_chg = movie_changes_result[int("%s" % movie_changes_index)]
-    td_movie_name[2] = movie_chg
+    td_movie_name[2] = movie_changes_result[movie_changes_index]
     movie_name_result.append(td_movie_name)
     rank += 1
     movie_changes_index += 1
