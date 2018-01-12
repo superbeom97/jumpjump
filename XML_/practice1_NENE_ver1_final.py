4322e80048d543023780bb278b9d53aa661e2772
IoT_class_beom <beom9790@naver.com>
2018-01-12 09:42:23 +0900

    수업_18.01.12

--- XML_/practice1_NENE_ver1_final.py
+++ XML_/practice1_NENE_ver1_final.py
@@ -12,8 +12,9 @@ text_name = "index_count.txt"
 csv_name = ".csv"
 
 def make_dir():
-    make_dir_name = dir_name
-    os.mkdir(make_dir_name)
+    # make_dir_name = dir_name
+    # os.mkdir(make_dir_name)
+    os.mkdir(dir_name)
     with open(dir_name + dir_rest + text_name, 'w') as text:
         text.write("0")
 
@@ -21,7 +22,7 @@ def make_file():
     with open(dir_name + dir_rest + text_name, 'r') as text:
         index_count = text.readline()
         count = int(index_count) + 1
-        make_file_name = dir_name + dir_rest + file_name + str("%s" % count) + csv_name
+        make_file_name = dir_name + dir_rest + file_name + str(count) + csv_name
         nene_table.to_csv(make_file_name, encoding="cp949", mode='w', index=True)
 
     with open(dir_name + dir_rest + text_name, 'w') as text:
@@ -38,16 +39,14 @@ for element in root.findall('item'):
     store_address = element.findtext('aname5')
     result.append([store_name]+[store_sido]+[store_gungu]+[store_address])
 
-nene_table = DataFrame(result,columns=('sotre','sido','gungu','store_address'))
+nene_table = DataFrame(result,columns=('store','sido','gungu','store_address'))
 
 
-while True:
-    if os.path.isdir(dir_name):
-        make_file()
-        break
-    elif not os.path.isdir(dir_name):
-        make_dir()
-        make_file()
-        break
+if os.path.isdir(dir_name):
+    make_file()
+
+elif not os.path.isdir(dir_name):
+    make_dir()
+    make_file()
 
 print("END!!!!")
