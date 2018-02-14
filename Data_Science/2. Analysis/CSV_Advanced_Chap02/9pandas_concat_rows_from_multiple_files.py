9477a0948a58aaa0148dfe1ec23e5fd17b2468ed
IoT_class_beom <beom9790@naver.com>
2018-02-14 13:54:23 +0900

    18.02.14

--- Data_Science/2. Analysis/CSV_Advanced_Chap02/9pandas_concat_rows_from_multiple_files.py
+++ Data_Science/2. Analysis/CSV_Advanced_Chap02/9pandas_concat_rows_from_multiple_files.py
@@ -5,7 +5,7 @@
 ## 'Run >> Edit Configurations'에서 'Script parameters'에
 ## -> "D:\Python_workspace\jumpjump\Data_Science\2. Analysis\CSV_Advanced_Chap02" output_files\\9output
 
-## (팬더스) 기본 프로세스는 각 입력 파일을 데터프레임으로 읽어 들이고 all_data_frame 리스트에 추가한 다음
+## (팬더스) 기본 프로세스는 각 입력 파일을 데이터프레임으로 읽어 들이고 all_data_frame 리스트에 추가한 다음
 ## concat() 함수를 사용하여 모든 데이터프레임을 하나의 데이터프레임으로 합친다.
 ## concat() 함수는 axis 인수를 통해서 데이터프레임을 병합하는 방향을 수직(axis=0) 또는 수평(axis=1)으로 지정할 수 있다.
 
