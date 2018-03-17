74fa1c538bb9944d51c2d75896defda45ec5af1e
subeom_home <seho1009@naver.com>
2018-03-17 13:11:33 +0900

    18.03.17_home

--- 03_Data_Science/3_Statistics/statistics_Chap07/304_wine_quality.py
+++ 03_Data_Science/3_Statistics/statistics_Chap07/304_wine_quality.py
@@ -14,18 +14,22 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from statsmodels.formula.api import ols, glm
 
-# 데이터셋을 팬더스 데이터프레임으로 읽기    ## ↱ 필드 구분 기호를 쉼표(sep=',')로 지정 // header=0 : 열 헤더를 파일 첫 행으로 지정
+
+##################################### 데이터셋을 팬더스 데이터프레임으로 읽기    ## ↱ 필드 구분 기호를 쉼표(sep=',')로 지정 // header=0 : 열 헤더를 파일 첫 행으로 지정
 wine = pd.read_csv('winequality-both.csv', sep=',', header=0)   ## pandas의 read_csv() 함수를 사용하여 CSV 파일을 읽어 데이터프레임에 넣는다.
 wine.columns = wine.columns.str.replace(' ', '_')   ## 열 헤더에 포함되어 있는 공백을 밑줄로 바꾼다(fixed acidity -> fixed_acidity)
 print(wine.head())  ## head() : 헤더 행과 처음 5개 행을 출력 -> () 안에 숫자를 넣으면, 그 숫자만큼 출력
 
-# 변수별 요약통계 표시
+
+##################################### 변수별 요약통계 표시
 print(wine.describe())  ## describe() : 수치형 변수들에 대한 요약통계를 출력
 # ↳ 관측값 개수(count), 평균(mean), 표준편차(std), 최솟값(min)
 #   25번째 백분위 수(25%), 중앙값(50%), 75번째 백분위 수(75%), 최댓값(max)
 
-# 유일값 찾기
+
+##################################### 유일값 찾기
 print(sorted(wine.quality.unique()))    ## quality.unique() : 유일값 출력
 
-# 빈도 계산
+
+##################################### 빈도 계산
 print(wine.quality.value_counts())    ## quality.value_counts() : 유일값별 관측값 개수를 내림차순으로 정렬하여 출력
